979a9e417f0b5c8c251b36036430556fdad13800
Bhimeswararao Matsa <bhimeswararao.matsa@sima.ai>
2025-01-16 14:54:50 +0530

    SiMa.AI Release 1.5.0

--- recipes-core/images/files/update.sh
+++ recipes-core/images/files/update.sh
@@ -6,6 +6,17 @@ UBOOT_ENV_DEV=/dev/mmcblk0p3
 UBOOT_ENV_MOUNT=/tmp/boot
 NO_REBOOT_FILE=/tmp/no_reboot
 
+
+#Build details
+cur_build_version=0
+cur_sdk_version=0
+cur_branch_name=0
+cur_build_number=0
+install_build_version=0
+install_sdk_version=0
+install_branch_name=0
+install_build_number=0
+
 if [ $# -lt 1 ]; then
         exit 0;
 fi
@@ -45,6 +56,36 @@ function get_update_block_device() {
 	fi
 }
 
+function get_current_build_version() {
+        cur_build_version=$(grep "SIMA_BUILD_VERSION" /etc/build | cut -d "=" -f 2)
+        echo "build version:${cur_build_version}"
+        cur_sdk_version=$( echo ${cur_build_version}| cut -d "_" -f 1)
+        cur_sdk_version=${cur_sdk_version//.}
+        cur_branch_name=$( echo ${cur_build_version}| cut -d "_" -f 2)
+        cur_build_number=$( echo ${cur_build_version}| cut -d "_" -f 3)
+        echo "Current sdk:${cur_sdk_version}, branch:${cur_branch_name}, build:${cur_build_number}"
+
+}
+
+function get_updated_build_version() {
+       mkdir -p /tmp/alt_rootfs
+	echo Updating ${UPDATE_ROOT}...
+       mount -t ext4 ${UPDATE_ROOT} /tmp/alt_rootfs
+       install_build_version=$(grep "SIMA_BUILD_VERSION" /tmp/alt_rootfs/etc/build | cut -d "=" -f 2)
+       umount /tmp/alt_rootfs
+       rm -rf /tmp/alt_rootfs
+        #install_build_version=1.4.0_master_B1160
+        #install_build_version=1.4.0_master_PRIVATE
+        echo "install_build version:${install_build_version}"
+        install_sdk_version=$( echo ${install_build_version}| cut -d "_" -f 1)
+        install_sdk_version=${install_sdk_version//.}
+        install_branch_name=$( echo ${install_build_version}| cut -d "_" -f 2)
+        install_build_number=$( echo ${install_build_version}| cut -d "_" -f 3)
+        echo "Install sdk:${install_sdk_version}, branch:${install_branch_name}, build:${install_build_number}"
+}
+
+
+
 if [ $1 == "preinst" ]; then
         # get current root device
         get_boot_device
@@ -71,20 +112,76 @@ if [ $1 == "preinst" ]; then
         ln -sf $UPDATE_ROOT /dev/update_root
         ln -sf $UPDATE_KERNEL /dev/update_kernel
         ln -sf $UPDATE_UBOOT /dev/update_uboot
+
+	#get current build details
+	get_current_build_version
 fi
 
 if [ $1 == "postinst" ]; then
-	fdt_addr=`(fw_printenv fdt_addr)`
-	if [ ${fdt_addr} != "0xAF000000" ]; then
-		fw_setenv fdt_addr 0xAF000000
-	fi
-	kernel_addr=`(fw_printenv kernel_addr)`
-	if [ ${kernel_addr} != "0xAA000000" ]; then
-		fw_setenv kernel_addr 0xAA000000
-	fi
         get_boot_device
         get_update_part
         get_update_block_device
+
+    if  [ -x "$(command -v resize2fs)" ]; then
+        resize2fs ${UPDATE_ROOT}
+    else
+        WARNING !! your rootfs was shrinked, install resize2fs to fix
+    fi
+	#get updated build details
+	get_updated_build_version
+
+	private_build=false
+	build_number=0
+	if [[ ${install_build_number:1} =~ ^[0-9]+$ ]]; then
+		build_number=${install_build_number:1}
+	else
+		private_build=true
+	fi
+
+	if [  ${install_sdk_version} -ge "150" ]; then
+		echo "updating required u-boot env parameters"
+		fdt_addr=`(fw_printenv fdt_addr)`
+		if [ ${fdt_addr} != "0x18F000000" ]; then
+			fw_setenv fdt_addr 0x18F000000
+		fi
+		kernel_addr=`(fw_printenv kernel_addr)`
+		if [ ${kernel_addr} != "0x18A000000" ]; then
+			fw_setenv kernel_addr 0x18A000000
+		fi
+		cpio_addr=`(fw_printenv cpio_addr)`
+		if [ ${cpio_addr} != "0x190000000" ]; then
+			fw_setenv cpio_addr 0x190000000
+		fi
+		scriptaddr=`(fw_printenv scriptaddr)`
+		if [ ${scriptaddr} != "0x180000000" ]; then
+			fw_setenv scriptaddr 0x180000000
+		fi
+		initrd_high=`(fw_printenv initrd_high)`
+		if [ ${initrd_high} != "0xFFFFFFFFFFFFFFFF" ]; then
+			fw_setenv initrd_high 0xFFFFFFFFFFFFFFFF
+		fi
+	else
+		echo "down grading required u-boot env parameters"
+		fdt_addr=`(fw_printenv fdt_addr)`
+		if [ ${fdt_addr} != "0xAF000000" ]; then
+			fw_setenv fdt_addr 0xAF000000
+		fi
+		kernel_addr=`(fw_printenv kernel_addr)`
+		if [ ${kernel_addr} != "0xAA000000" ]; then
+			fw_setenv kernel_addr 0xAA000000
+		fi
+		cpio_addr=`(fw_printenv cpio_addr)`
+		if [ ${cpio_addr} != "0xE0000000" ]; then
+			fw_setenv cpio_addr 0xE0000000
+		fi
+		scriptaddr=`(fw_printenv scriptaddr)`
+		if [ ${scriptaddr} != "0x90000000" ]; then
+			fw_setenv scriptaddr 0x90000000
+		fi
+			fw_setenv initrd_high
+
+	fi
+
 	if [ $UPDATE_KERNEL != $UBOOT_ENV_DEV ]; then
 		mount $UPDATE_KERNEL ${UPDATE_MOUNT_PATH}
 	fi
@@ -114,12 +211,6 @@ if [ $1 == "postinst" ]; then
         if [[ -h "/dev/update_uboot" ]]; then
                 rm /dev/update_uboot
         fi
-
-    if  [ -x "$(command -v resize2fs)" ]; then
-        resize2fs ${UPDATE_ROOT}
-    else
-        WARNING !! your rootfs was shrinked, install resize2fs to fix
-    fi
 
 	BOOT_DEVICE=${ROOT::-2}
 	echo boot device:${BOOT_DEVICE}
